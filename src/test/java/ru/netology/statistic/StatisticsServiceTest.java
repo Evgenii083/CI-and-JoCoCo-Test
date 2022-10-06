ee5760985b7c65209a2b05c468912de6c51090e5
Evgenii Rybalkin <eugeny.rybalkin@gmail.com>
2022-10-06 11:18:34 +0200

    add 100% testing code coverage

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,29 @@ public class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    void anotherArrayForFindMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomeInBillions = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+        long expected = 13;
+
+        long actual = service.findMax(incomeInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+
+
+    @Test
+    void currentMaxMoreThanIncome() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomeInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 3};
+        long expected = 12;
+        long actual = service.findMax(incomeInBillions);
+
+        assertEquals(expected, actual);
+    }
 }
